0fc5e708f1e346105af61b571180f44e3d9b5b97
Carlos Tan <carlos.quinto.t@gmail.com>
2021-12-03 00:26:17 +0100

    Add solution day02 part1

---
+++ day02/index.mjs
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+
+const test = [
+  "forward 5",
+  "down 5",
+  "forward 8",
+  "up 3",
+  "down 8",
+  "forward 2",
+];
+
+const positions = fs.readFileSync("./positions.txt", "utf-8").split(/\n/);
+
+function calculateDive01(positions) {
+  let horizontal = 0;
+  let vertical = 0;
+  positions.forEach((pos) => {
+    if (pos.includes("forward")) {
+      horizontal += Number(pos.replace("forward ", ""));
+    } else if (pos.includes("down")) {
+      vertical += Number(pos.replace("down ", ""));
+    } else if (pos.includes("up")) {
+      vertical -= Number(pos.replace("up ", ""));
+    }
+  });
+
+  return horizontal * vertical;
+}
+
+console.log(calculateDive01(positions)); // 1694130
