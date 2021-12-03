95dd9f2153445a76a738f343d155e3ac2ff0c3fa
Carlos Tan <carlos.quinto.t@gmail.com>
2021-12-03 23:50:26 +0100

    Add solution day03 part2

--- day03/index.mjs
+++ day03/index.mjs
@@ -9,6 +9,10 @@ function transpose(matrix) {
   return matrix[0].map((_, i) => matrix.map((row) => row[i]));
 }
 
+function getDecimal(numArray) {
+  return parseInt(numArray.toString().replaceAll(",", ""), 2);
+}
+
 const gammaBin = transpose(input).reduce((prev, curr) => {
   if (curr.reduce((prev, curr) => prev + curr, 0) >= input.length / 2) {
     return [...prev, 1];
@@ -16,8 +20,8 @@ const gammaBin = transpose(input).reduce((prev, curr) => {
 }, []);
 const epsilonBin = gammaBin.map((g) => Number(!Boolean(g)));
 
-const gamma = parseInt(gammaBin.toString().replaceAll(",", ""), 2);
-const epsilon = parseInt(epsilonBin.toString().replaceAll(",", ""), 2);
+const gamma = getDecimal(gammaBin);
+const epsilon = getDecimal(epsilonBin);
 console.log(`Part1: ${gamma * epsilon}`);
 
 /*
@@ -41,15 +45,11 @@ const test = [
 
 function filterByBinary(input, binary, type) {
   let final = [...input];
-  let isDone = false;
   binary.forEach((bin, idx) => {
-    if (!isDone) {
-      if (isBinarySameLength(final, idx, type)) {
-        final = final.filter((data) => data[idx] === 1);
-        done = true;
-      } else {
-        final = final.filter((data) => data[idx] === bin);
-      }
+    if (isBinarySameLength(final, idx, type)) {
+      final = final.filter((data) => data[idx] === (type === "gamma" ? 1 : 0));
+    } else if (final.length > 1) {
+      final = final.filter((data) => data[idx] === bin);
     }
   });
 
@@ -57,36 +57,8 @@ function filterByBinary(input, binary, type) {
 }
 
 function isBinarySameLength(data, idx, type) {
-  console.log("🚀 ~ file: index.mjs ~ line 60 ~ isBinarySameLength ~ idx", idx);
-  console.log(
-    "🚀 ~ file: index.mjs ~ line 60 ~ isBinarySameLength ~ data",
-    data
-  );
   const tmpData = transpose(data)[idx];
   const filteredData = transpose(data)[idx].filter((d) => d === 1);
   const isSameLength = filteredData.length === tmpData.length / 2;
   return isSameLength ? (type === "gamma" ? 1 : 0) : false;
 }
-
-// console.log(filterByBinary(test, [1, 0, 1, 1, 0]));
-// console.log(
-//   parseInt(
-//     filterByBinary(test, [1, 0, 1, 1, 0], "gamma")
-//       .toString()
-//       .replaceAll(",", ""),
-//     2
-//   )
-// );
-
-console.log(
-  parseInt(
-    filterByBinary(test, [0, 1, 0, 0, 1], "epsilon")
-      .toString()
-      .replaceAll(",", ""),
-    2
-  )
-);
-
-// console.log(
-//   parseInt(filterByBinary(input, gammaBin).toString().replaceAll(",", ""), 2)
-// );
